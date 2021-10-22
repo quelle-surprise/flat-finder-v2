0520aa8fba654a2dd32e35964042ad43084d3373
QuelleSurprise <jakubpiotrowski1@hotmail.com>
2021-10-22 19:47:26 +0200

    init

---
+++ src/main/java/com/quellesup/flatfinder/FlatFinderApplication.java
@@ -0,0 +1,13 @@
+package com.quellesup.flatfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlatFinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quellesup/flatfinder/FlatFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quellesup.flatfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
